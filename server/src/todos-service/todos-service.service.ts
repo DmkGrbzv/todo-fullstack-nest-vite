89b2b3a8bbe5bf5d761a00c61095b9f1288e6a79
Garbuzov Dmitriy <harbuzov.dima97@gmail.com>
2022-08-28 13:39:30 +0300

    S-create. Created methods patch, delete, post, getAll

--- server/src/todos-service/todos-service.service.ts
+++ server/src/todos-service/todos-service.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Todo, TodoDocument } from 'src/todos/shemas/todo.shema';
+import { CreateTaskDto } from 'src/todos/dto/create-task.dto';
+import { UpdateTaskDto } from 'src/todos/dto/update-task.dto';
 
 @Injectable()
 export class TodosService {
-  getAll(){
-    return 'hello'
+  constructor(@InjectModel(Todo.name) private todoModel: Model<TodoDocument>) {}
+  async getAll(): Promise<Todo[]> {
+    return await this.todoModel.find().exec();
+  }
+  async create(taskDto: CreateTaskDto): Promise<Todo> {
+    const newTask = new this.todoModel(taskDto);
+    return newTask.save();
+  }
+  async update(id: string, taskDto: UpdateTaskDto): Promise<Todo> {
+    await this.todoModel.findByIdAndUpdate({ _id: id }, taskDto);
+    return null;
+  }
+  async remove(id: string): Promise<Todo> {
+    return await this.todoModel.findByIdAndRemove(id);
   }
 }
--- server/src/todos/todos.controller.ts
+++ server/src/todos/todos.controller.ts
@@ -1,26 +1,41 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Req, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
-import { Request, Response } from 'express';
+// import { Request, Response } from 'express';
 import { TodosService } from 'src/todos-service/todos-service.service';
+import { Todo } from './shemas/todo.shema';
 
 @Controller('todos')
 export class TodosController {
-  constructor(private readonly todosService:TodosService){}
-  @Get() 
-  getAllTasksById(@Req() req: Request, @Res() res:Response):string{
+  constructor(private readonly todosService: TodosService) {}
+  @Get('getAll')
+  getAllTasks(): Promise<Todo[]> {
     return this.todosService.getAll();
-  } 
-  @Post()
-  createTask(@Body() CreateTaskDto: CreateTaskDto):string{
-    return `${CreateTaskDto.id} ${CreateTaskDto.text} ${CreateTaskDto.completed}`
   }
-  @Patch(':id')
-  updateTask(@Body() updateTaskDto: UpdateTaskDto, @Param(':id') id:string):string{
-    return `${updateTaskDto.text} ${updateTaskDto.id} ${updateTaskDto.completed}`
+  @Post('create')
+  createTask(@Body() CreateTaskDto: CreateTaskDto) {
+    return this.todosService.create(CreateTaskDto);
   }
-  @Delete(':id')
-  removeTask(@Param('id') id:string){
-    return `remove ${id}`
+  @Patch('update/:id')
+  async updateTask(
+    @Body() UpdateTaskDto: UpdateTaskDto,
+    @Param('id') id: string,
+  ) {
+    const changedTask = await this.todosService.update(id, UpdateTaskDto);
+    return changedTask;
+  }
+  @Delete('delete/:id')
+  removeTask(@Param('id') id: string) {
+    return this.todosService.remove(id);
   }
 }
