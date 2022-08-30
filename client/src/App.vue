f98d70a9744f695ed31f3d365136acf9def4ae5a
Garbuzov Dmitriy <harbuzov.dima97@gmail.com>
2022-08-30 18:07:29 +0300

    C-create. Created TodoItem, TodoInput, Checkbox components/Styled components

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,13 +2,17 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 
-import CheckBox from './components/CheckBox.vue';
+import TodoInput from './components/TodoInput.vue';
+import TodoItem from './components/TodoItem.vue';
 </script>
 
 <template>
   <div class="wrapper">
+    <div class="title">To-do list</div>
     <!-- <HelloWorld msg="Vite + Vue" /> -->
     <CheckBox/>
+    <TodoInput/>
+    <TodoItem/>
   </div>
 </template>
 
--- client/src/components/TodoInput.vue
+++ client/src/components/TodoInput.vue
@@ -13,9 +13,14 @@
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  
+  <div class="input-wrapper">
+    <label for="" class="label">
+      <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M20 8.75H11.25V0H8.75V8.75H0V11.25H8.75V20H11.25V11.25H20V8.75Z" fill="#7A7C8D"/>
+      </svg>
+      <input type="text" />
+    </label>
+  </div>
 </template>
 
 <style scoped>
