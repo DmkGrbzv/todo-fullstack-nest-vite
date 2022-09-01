433337dd0c6521ef0fce41a4137c1e1367d414ab
Garbuzov Dmitriy <harbuzov.dima97@gmail.com>
2022-09-01 14:24:23 +0300

    C-create. Added completed list label

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,5 @@
 <script lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+
 import { defineComponent } from "@vue/runtime-core"
 import { mapActions, storeToRefs } from 'pinia';
 import TodoInput from './components/TodoInput.vue';
@@ -54,9 +53,17 @@ export default defineComponent({
     <div class="title">To-do list</div>
     <TodoInput :create="createTask"/>
     <TodoItemList :itemsProp = 'unCompletedTasks' v-show="unCompletedTasks.length"/>
-    <div v-show="completedTasks.length">Completed</div>
+
+    <div class="vertical-line"></div>
+
+    <div class="label-wrapper" v-show="completedTasks.length">
+      <svg width="12" height="11" viewBox="0 0 12 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M10.0113 1.19614L4.38823 7.17566L1.98874 4.62391C1.53376 4.14022 0.796064 4.14022 0.341237 4.62391C-0.113746 5.10793 -0.113746 5.89227 0.341237 6.37612L3.56448 9.8039C3.79189 10.0457 4.09014 10.1667 4.38823 10.1667C4.68633 10.1667 4.98457 10.0457 5.21198 9.8039L11.6586 2.94835C12.1138 2.4645 12.1138 1.67999 11.6588 1.1963C11.204 0.712448 10.4663 0.712282 10.0113 1.19614Z" fill="#4EA2FF"/>
+      </svg>  
+      <div class="completed-list-label">Completed</div>
+    </div>
+    
     <TodoItemList :itemsProp = 'completedTasks' v-show="completedTasks.length"/>
-    <!-- <TodoItemList/> -->
   </div>
 </template>
 
